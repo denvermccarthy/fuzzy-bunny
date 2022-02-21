9bb44a5716444ddbef5b34e51b2d492ba6d1c5cb
Denver McCarthy <dmichaelspdx@gmail.com>
2022-02-21 14:03:30 -0800

    added select options

--- create/create.js
+++ create/create.js
@@ -8,24 +8,34 @@ import {
 const form = document.querySelector('.bunny-form');
 const logoutButton = document.getElementById('logout');
 
-form.addEventListener('submit', async e => {
+form.addEventListener('submit', async (e) => {
     // prevent default
-
+    e.preventDefault();
     // get the name and family id from the form
+    const data = new FormData(form);
 
+    const bunny = {
+        name: data.get('bunny-name'),
+        family_id: data.get('family-id')
+    };
     // use createBunny to create a bunny with this name and family id
-    
+    console.log(bunny);
     form.reset();
 });
 
-window.addEventListener('load', async() => {
+window.addEventListener('load', async () => {
     // let's dynamically fill in the families dropdown from supabase
     // grab the select HTML element from the DOM
-
+    const famSelect = document.getElementById('family-select');
     // go get the families from supabase
-
+    const families = await getFamilies();
     // for each family
-
+    families.forEach(element => {
+        const option = document.createElement('option');
+        option.value = element.id;
+        option.textContent = element.name;
+        famSelect.append(option);
+    });
     // create an option tag
 
     // set the option's value and text content
